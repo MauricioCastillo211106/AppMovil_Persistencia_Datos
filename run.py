8433d644d95fbe71d1bed9f1acbd814d5eb99f2e
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2024-05-21 10:51:10 -0600

    add course

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
 from database import create_app, db
 from app.routes import register_routes
 
 app = create_app()
+CORS(app)  # Habilitar CORS para toda la aplicación
 
 with app.app_context():
     db.create_all()
