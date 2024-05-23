a64437e6fce75a1e24634a03ccbaa96513318459
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2024-05-22 18:49:52 -0600

    Conection from db

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:040902@localhost:5432/products_db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://doadmin:AVNS_T7V30HCrgi3TBQX95VW@db-postgresql-nyc3-16736-do-user-15995791-0.c.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
