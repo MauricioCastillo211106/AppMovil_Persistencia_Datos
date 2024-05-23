9e7a9f7732fd298e0ff53eb208aa0713dafeadc2
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2024-05-22 18:56:44 -0600

    add environment

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://doadmin:AVNS_T7V30HCrgi3TBQX95VW@db-postgresql-nyc3-16736-do-user-15995791-0.c.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    load_dotenv()  
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
